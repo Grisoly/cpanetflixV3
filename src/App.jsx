b1968bcc4c3df1a26910d4dd98e717ece344ee07
Grisoly <120173770+Grisoly@users.noreply.github.com>
2023-12-11 13:30:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Row from './Row'
+import './App.css'
+import requests from './requests';
+import Banner from './Banner';
+
+function App() {
+  
+  return (
+    <div className='app'>
+      {/* NavBar */}
+      {/* Banner */}
+      <Banner/>
+      <Row title="CPANETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+    );
+}
+
+export default App;
